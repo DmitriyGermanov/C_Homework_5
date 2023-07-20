967579063c85bf8838c99714fd62c3b5b1533c79
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-07-20 21:40:23 +0300

    changes for first task

--- task00/Program.cs
+++ task00/Program.cs
@@ -2,32 +2,30 @@
 //  Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-
-using System.Collections.Concurrent;
-using System.Runtime.CompilerServices;
-
 int[] Input(string abc) // Записываем массив из строки, преобразуем строковой массив в числовой//На вход принимаем сообщение для пользователя
 {
     Console.Write($"{abc} ");
     string[] input = Console.ReadLine()!.Split(',');
     int[] numbers = new int[input.Length];
-    Console.WriteLine(input.Length);
     for (int i = 0; i < input.Length; i++)
         numbers[i] = Convert.ToInt32(input[i]);
 
     return numbers;
 }
 
-int[] array = Input("Введите трехзначные числа через запятую (без пробела)");
-
-int countA = 0; //Счетчик для четных
-int countB = 0; //Счетчик для нечетных
-
-foreach (int i in array)
+int[] EvenOrOdd(int[] array) //Функция нахождения количества четных и нечетных элементов массива, на выходе выдает массив где элемент [0] - количество четных, элемент [1] - количество нечетных;
 {
-    if (i % 2 == 0)
-        countA += 1;
-    else
-        countB += 1;
+    int[] count = new int[2];
+
+    foreach (int i in array)
+    {
+        if (i % 2 == 0)
+            count[0] += 1;
+        else
+            count[1] += 1;
+    }
+    return count;
 }
-Console.WriteLine($"Количество четных элементов = {countA} \nКоличество нечетных элементов = {countB}");
+
+int[] array = Input("Введите трехзначные числа через запятую (без пробела)");
+Console.WriteLine($"Количество четных элементов = {EvenOrOdd(array)[0]} \nКоличество нечетных элементов = {EvenOrOdd(array)[1]}");
